8fabadd789daed1a849a44fa5c8c91553872db34
MarlyKhoury <marlykh625@gmail.com>
2022-01-23 17:59:50 +0200

    fixed the boundary colors

--- game.js
+++ game.js
@@ -1,14 +1,19 @@
+
 window.onload=(event)=>{
 
     document.getElementById("start").onclick = gameReset
     document.getElementById("end").addEventListener("mouseover",gameEnd)
+    
     for(var i=0;i<document.getElementsByClassName("boundary").length;i++){
-    document.getElementsByClassName("boundary")[i].addEventListener("mouseover",accident)
+    document.getElementsByClassName("boundary")[i].addEventListener("mouseover",accident)    
     }
+  
 
 var addition=5
 var subtraction=-10
 var total=0
+
+
 
 function gameReset() {
      var boundary=document.getElementsByClassName("boundary")
@@ -17,33 +22,40 @@ function gameReset() {
    }
    document.getElementById("status").innerHTML='Begin by moving your mouse over the "S."'
    total= 0
+
 }
 
 function accident() {
     var boundary=document.getElementsByClassName("boundary")
   for(var i=0;i<boundary.length;i++){
-    boundary[i].style.background="red"
+    boundary[i].style.background="#ff8888"
+    
      }
   document.getElementById("status").innerHTML="You Lost"
   total += subtraction
   setTimeout(function() {
     alert ("You Lost! Total points earned "+ total)
   },1)
-   }
+}
 
 
 function gameEnd(){
   if (document.getElementById("status").innerHTML!="You Lost"){
     document.getElementById("status").innerHTML="You Won"
     total+= addition
+    
   }
   var boundary=document.getElementsByClassName("boundary")
   for(var i=0;i<boundary.length;i++){
-    boundary[i].style.background="green"
+    boundary[i].style.background="#88ff88"
+    
   }
+  
+isStart = false;
   setTimeout(function(){
   alert ("You Won! Total points earned " + total)
   },1)
 }
+
 
 }
