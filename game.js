5ad2f50ae20cfa6926fb01c56038c0d54804febf
MarlyKhoury <marlykh625@gmail.com>
2022-01-24 12:44:40 +0200

    fixed the cheating

--- game.js
+++ game.js
@@ -1,17 +1,20 @@
 
 window.onload=(event)=>{
 
+    document.getElementById("start").addEventListener("mouseover",startAgain)
     document.getElementById("start").onclick = gameReset
-    document.getElementById("end").addEventListener("mouseover",gameEnd)    
+    document.getElementById("end").addEventListener("mouseover",gameEnd)
+    document.getElementById("game").addEventListener("mouseleave",cheating)    
     for(var i=0;i<document.getElementsByClassName("boundary").length;i++){
-    document.getElementsByClassName("boundary")[i].addEventListener("mouseover",accident)    
+      document.getElementsByClassName("boundary")[i].addEventListener("mouseover",accident)    
     }
-  
+     
+   
 
 var addition=5
 var subtraction=-10
 var total=0
-
+var out_bound=false
 
 
 function gameReset() {
@@ -39,20 +42,41 @@ function accident() {
 }
 
 function gameEnd(){
-  if (document.getElementById("status").innerHTML!="You Lost"){
-    document.getElementById("status").innerHTML="You Won"
-    total+= addition
-    
-  }
-  var boundary=document.getElementsByClassName("boundary")
-  for(var i=0;i<boundary.length;i++){
-    boundary[i].style.background="#88ff88"
-  }
-  setTimeout(function(){
-  alert ("You Won! Total points earned " + total)
-  },1)
+  if ((document.getElementById("status").innerHTML!="You Lost")&&(out_bound==false)){
+      document.getElementById("status").innerHTML="You Won"
+      total+= addition 
+  
+      var boundary=document.getElementsByClassName("boundary")
+      for(var i=0;i<boundary.length;i++){
+        boundary[i].style.background="#88ff88"
+      }
+        setTimeout(function(){
+          alert ("You Won! Total points earned " + total)
+        },1)
+
+    }
+  
+  else {document.getElementById("status").innerHTML="Nice Try"}
+
 }
+
+
+function startAgain(){
+  var boundary= document.getElementsByClassName("boundary")
+  for (var i=0; i<boundary.length;i++){
+    boundary[i].style.background="#eeeeee"
+  }
+  document.getElementById("status").innerHTML='Begin by moving your mouse over the "S."'
+  out_bound=false
+
+}
+
+
+function cheating(){
+  out_bound=true
+  
 }
 
 
 
+}
